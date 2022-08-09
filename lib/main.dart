4dc67106fca4ad032a8c834bd6f1632cfe06384d
Arsen Ghazaryan <arsen.ghazaryan@gmail.com>
2022-08-09 11:36:13 -0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const FlutterImage(),
+    );
+  }
+}
+
+class FlutterImage extends StatelessWidget {
+  const FlutterImage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset('assets/background.png'),
+      ),
+    );
+  }
+}
